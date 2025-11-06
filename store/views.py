b07540244cd3330434ba758a25df08c143b85ea9
Denn-oru <denisseadrianadominguezroblero@gmail.com>
2025-11-06 12:03:45 -0800

    Agregue el proyecto marketplace_main con la aplicacion store

---
+++ store/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from.models import Item, Category
+from django.shortcuts import get_object_or_404
+
+# Create your views here.
+def home(request):
+    Items = Item.objects.filter(is_solid=False)
+    Categories = Category.objects.all()
+    context={
+        'items': Items,
+        'categories': Categories
+    }
+    return render(request,'store/home.html', context)
+
+def contact(request):
+    context ={
+        'msg':'¿Quieres otros productos? Contactame'
+    }
+    return render(request,'store/contact.html',context)
+
+def detail(request, pk):
+    item=get_object_or_404(Item, pk=pk)
+    related_items=Item.objects.filter(category=item.category, is_solid=False).exclude(pk=pk)[0:3]
+
+    context={
+        'Item':item,
+        'related_items': related_items
+    }
+    return render(request,'store/item.html',context)
---
+++ sttore/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SttoreConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'sttore'
